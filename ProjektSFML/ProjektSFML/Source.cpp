eae4a8da980693bdeef88615a783b65fbc3f43fa
OtakuFreak13 <andromeda_stargate@hotmail.com>
2017-02-10 09:40:55 +0100

    Fixat stationära datorn hemma och gjort ett eget wiondow

--- ProjektSFML/ProjektSFML/Source.cpp
+++ ProjektSFML/ProjektSFML/Source.cpp
@@ -4,24 +4,48 @@
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+	//sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+	//sf::CircleShape shape;
+	//shape.setRadius(40.f);
+	//shape.setPosition(100.f, 100.f);
+	//shape.setFillColor(sf::Color::Cyan);
+
+	//while (window.isOpen()) {
+
+	//	sf::Event event;
+	//	while (window.pollEvent(event)) {
+
+	//		if (event.type == sf::Event::Closed)
+	//			window.close();
+	//	}
+
+	//	window.clear();
+	//	window.draw(shape);
+	//	window.display();
+	//}
+
+
+
+
+	sf::RenderWindow renderWindow(sf::VideoMode(200, 200), "SFML Demo");
+	sf::Event event;
 	sf::CircleShape shape;
-	shape.setRadius(40.f);
-	shape.setPosition(100.f, 100.f);
-	shape.setFillColor(sf::Color::Cyan);
+	shape.setRadius(50.f);
+	shape.setPosition(50.f, 50.f);
+	shape.setFillColor(sf::Color::Magenta);
 
-	while (window.isOpen()) {
-
-		sf::Event event;
-		while (window.pollEvent(event)) {
-
-			if (event.type == sf::Event::Closed)
-				window.close();
+	while (renderWindow.isOpen())
+	{
+		while (renderWindow.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed)
+			{
+				renderWindow.close();
+			}
 		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
+		renderWindow.clear();
+		renderWindow.draw(shape);
+		renderWindow.display();
 	}
 
 }
